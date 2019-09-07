d5fb3056dacc29c3906b39ad653616e0d5cebf8f
Jeffrey Layfield <jlayfield85@gmail.com>
2019-09-07 19:33:27 -0400

    updating orm and index.html

--- config/orm.js
+++ config/orm.js
@@ -5,7 +5,13 @@ var orm = {
         connection.query('SELECT * FROM '+tableInput+';', function(err, result){
             if(err) throw err; cb (result)
         })
+    },
+
+    update: function(tableInput, condition, cb){
+     connection.query('UPDATE '+tableInput+';' ''SET devoured=true WHERE id=+condition+';', function(err, result){
+           if(err)throw err;
+            cb(result);
+       })
     }
 }
-
 module.exports = orm;
